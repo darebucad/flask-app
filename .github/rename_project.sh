d04ca6f001d22b56f9d1241bd6155480d7ae14fa
darebucad <darebucad@users.noreply.github.com>
2023-10-01 02:20:48 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="darebucad"
+original_name="flask_app"
+original_urlname="flask-app"
+original_description="Awesome flask_app created by darebucad"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv flask_app $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
